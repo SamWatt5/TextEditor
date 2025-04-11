01e30490c0019507da6da92cd79a49bf30f5f466
SamWIsALegend <notchnotch5@gmail.com>
2025-04-11 21:34:49 +0100

    window!

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
+#include <FL/Fl_Multiline_Input.H>
 #include <FL/Fl_Box.H>
 
 int main() {
-	Fl_Window* window = new Fl_Window(400, 300, "Hello World");
-	Fl_Box* box = new Fl_Box(0, 0, 400, 300, "Hello, World!");
-	box->labelsize(36);
-	box->labeltype(FL_SHADOW_LABEL);
-	window->resizable(box);
+	int width = 400;
+	int height = 300;
+	Fl_Window* window = new Fl_Window(width, height, "Hello World");
+	Fl_Multiline_Input* multilineInput = new Fl_Multiline_Input(0, 0, width, height, "Hello World! :)");
+	multilineInput->textfont(FL_COURIER);
+	multilineInput->textsize(16);
+	multilineInput->textcolor(FL_BLACK);
 	window->end();
 	window->show();
 	return Fl::run();
